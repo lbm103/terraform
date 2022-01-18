18278b75af8e810aaec27c6add42f44092226e16
lbm103 <mike103@kuberix.com>
2022-01-18 16:35:49 +0900

    220118-autoscaling

--- main.tf
+++ main.tf
@@ -2,10 +2,51 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
-resource "aws_instance" "example" {
-  ami		= "ami-0ed11f3863410c386"
+data "aws_availability_zones" "all" {}
+
+variable "server_port" {
+  default	= 8080
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port	= "${var.server_port}"
+    to_port	= "${var.server_port}"
+    protocol	= "tcp"
+    cidr_blocks	= ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_launch_configuration" "example" {
+  image_id		= "ami-0ed11f3863410c386"
   instance_type	= "t2.micro"
-  tags = {
-    Name = "terraform-example"
+  security_groups = ["${aws_security_group.instance.id}"]
+
+  user_data = <<-EOF
+                  #!/bin/bash
+                  echo "Hello, World" > index.html
+                  nohup busybox httpd -f -p "${var.server_port}" &
+                  EOF
+  lifecycle {
+   create_before_destroy = true
   }
 }  
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = "${aws_launch_configuration.example.id}"
+  availability_zones = "${data.aws_availability_zones.all.names}"
+  
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key	= "Name"
+    value	= "terraform-asg-example"
+    propagate_at_launch	= true
+  }
+}
