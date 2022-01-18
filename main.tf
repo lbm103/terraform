cb6ed552c803a6449d1903347e4b052e6c385a22
lbm103 <mike103@kuberix.com>
2022-01-18 17:11:30 +0900

    220118-elb

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ variable "server_port" {
 }
 
 resource "aws_security_group" "instance" {
-  name = "terraform-example-instance"
+  name = "terraform-example-instance2"
   ingress {
     from_port	= "${var.server_port}"
     to_port	= "${var.server_port}"
@@ -19,6 +19,23 @@ resource "aws_security_group" "instance" {
 
   lifecycle {
     create_before_destroy = true
+  }
+}
+
+resource "aws_security_group" "elb" {
+  name	= "terraform-example-elb"
+  ingress {
+    from_port	= 80
+    to_port	= 80
+    protocol	= "tcp"
+    cidr_blocks	= ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port	= 0
+    to_port	= 0
+    protocol	= "-1"
+    cidr_blocks	= ["0.0.0.0/0"]
   }
 }
 
@@ -35,11 +52,35 @@ resource "aws_launch_configuration" "example" {
   lifecycle {
    create_before_destroy = true
   }
+}
+
+resource "aws_elb" "example" {
+  name	= "terraform-asg-example"
+  availability_zones = "${data.aws_availability_zones.all.names}"
+  security_groups	= ["${aws_security_group.elb.id}"]
+
+  listener {
+    lb_port	= 80
+    lb_protocol	= "http"
+    instance_port	= "${var.server_port}"
+    instance_protocol = "http"
+  }
+
+  health_check {
+    healthy_threshold	= 2
+    unhealthy_threshold	= 2
+    timeout		= 3
+    interval		= 30
+    target			= "HTTP:${var.server_port}/"
+  }
 }  
 
 resource "aws_autoscaling_group" "example" {
   launch_configuration = "${aws_launch_configuration.example.id}"
   availability_zones = "${data.aws_availability_zones.all.names}"
+
+  load_balancers	= ["${aws_elb.example.name}"]
+  health_check_type	= "ELB"
   
   min_size = 2
   max_size = 10
@@ -49,4 +90,8 @@ resource "aws_autoscaling_group" "example" {
     value	= "terraform-asg-example"
     propagate_at_launch	= true
   }
+}
+
+output "elb_dns_name" {
+  value	= "${aws_elb.example.dns_name}"
 }
